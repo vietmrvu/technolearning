a247d525e0ca8111ef671fefa118d601f500007c
Vietcoder <sirdang@Macintoshs-MacBook.local>
2022-07-22 18:49:51 +0700

    Prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
-		? 'http://localhost:5000/api'
-		: 'DeployedUrl'
+		? 'https://gentle-fjord-05354.herokuapp.com/api'
+		: 'https://gentle-fjord-05354.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'technolearning'
 
